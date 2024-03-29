1c9b3cd6e98b3040bd22a21bf7095f8b600bb72e
Kamil <smolarekkamil123@gmail.com>
2024-03-29 07:54:43 +0100

    added liquibase

--- modules/activity-module/activity-rest/src/main/java/com/sensilabs/projecthub/activity/ActivityController.java
+++ modules/activity-module/activity-rest/src/main/java/com/sensilabs/projecthub/activity/ActivityController.java
@@ -1,6 +1,6 @@
 package com.sensilabs.projecthub.activity;
 
-import com.sensilabs.projecthub.activity.forms.CreateUserForm;
+import com.sensilabs.projecthub.activity.forms.*;
 import com.sensilabs.projecthub.activity.model.Activity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +17,31 @@ public class ActivityController {
         this.activityService = activityService;
     }
 
-//    @PostMapping
-//    public Activity saveActivity(@RequestBody CreateUserForm activity) {return activityService.save(activity);}
+    @PostMapping
+    public Activity save(@RequestBody CreateUserForm activity) {return activityService.save(activity, activity.getUserId());}
+
+    @PostMapping("/createUser")
+    public Activity saveCreateUser(@RequestBody CreateUserForm activity) {return activityService.save(activity, activity.getUserId());}
+
+    @PostMapping("/deleteUser")
+    public Activity saveDeleteUser(@RequestBody DeleteUserForm activity) {return activityService.save(activity, activity.getUserId());}
+
+    @PostMapping("/documentDownload")
+    public Activity saveDocumentDownload(@RequestBody DocumentDownloadForm activity) {return activityService.save(activity, activity.getUserId());}
+
+    @PostMapping("/documentOpen")
+    public Activity saveDocumentOpen(@RequestBody DocumentOpenForm activity) {return activityService.save(activity, activity.getUserId());}
+
+    @PostMapping("/keyOpen")
+    public Activity saveKeyOpen(@RequestBody KeyOpenForm activity) {return activityService.save(activity, activity.getUserId());}
+
+    @PostMapping("/logInFailed")
+    public Activity saveLogInFailed(@RequestBody LogInFailedUserForm activity) {return activityService.save(activity, activity.getUserId());}
+
+    @PostMapping("/logInSuccess")
+    public Activity saveLogInSuccess(@RequestBody LogInSuccessUserForm activity) {return activityService.save(activity, activity.getUserId());}
+
+    @PostMapping("/logOut")
+    public Activity saveLogOut(@RequestBody LogOutUserForm activity) {return activityService.save(activity, activity.getUserId());}
 
 }
