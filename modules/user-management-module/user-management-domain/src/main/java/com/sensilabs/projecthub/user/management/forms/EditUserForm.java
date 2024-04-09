f7ddd4f64262ed2c56840c19f2354735aabafc47
Kacper <109245559+kacpi-k@users.noreply.github.com>
2024-04-09 11:38:04 +0200

    Improved

--- modules/user-management-module/user-management-domain/src/main/java/com/sensilabs/projecthub/user/management/forms/EditUserForm.java
+++ modules/user-management-module/user-management-domain/src/main/java/com/sensilabs/projecthub/user/management/forms/EditUserForm.java
@@ -3,7 +3,7 @@ package com.sensilabs.projecthub.user.management.forms;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,9 +17,7 @@ import org.hibernate.validator.constraints.Length;
 public class EditUserForm {
 
     @NotNull(message = "ID must not be null")
-    //@Size(min = 36, max = 36, message = "\nID must be 36 characters long")
-    @Pattern(regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$",
-            message = "Wrong ID pattern!")
+    @Size(min = 36, max = 36, message = "\nID must be 36 characters long")
     private String id;
 
     @NotBlank(message = "First name must not be blank")
