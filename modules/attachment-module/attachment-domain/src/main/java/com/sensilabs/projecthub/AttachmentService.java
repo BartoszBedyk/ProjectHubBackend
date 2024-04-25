8517b3b1ec5aab7ea1b4a3819e6428b23e3e3e46
Damian Szymański <damian.szymanski@senislabs.pl>
2024-04-25 12:59:40 +0200

    Refactor attachment module

--- modules/attachment-module/attachment-domain/src/main/java/com/sensilabs/projecthub/AttachmentService.java
+++ modules/attachment-module/attachment-domain/src/main/java/com/sensilabs/projecthub/AttachmentService.java
@@ -1,8 +1,9 @@
 package com.sensilabs.projecthub;
 
-import org.springframework.web.multipart.MultipartFile;
+import java.io.InputStream;
 
 public interface AttachmentService {
-    Attachment save(MultipartFile file, String createdById);
-    Attachment getById(String id);
+	Attachment save(InputStream inputStream, String originalFilename, String createdById);
+
+	Attachment getById(String id);
 }
--- modules/attachment-module/attachment-infrastructure/src/main/java/com/sensilabs/projecthub/LocalStorageService.java
+++ modules/attachment-module/attachment-infrastructure/src/main/java/com/sensilabs/projecthub/LocalStorageService.java
@@ -1,11 +1,7 @@
 package com.sensilabs.projecthub;
 
-import com.sensilabs.projecthub.commons.ApplicationException;
-import com.sensilabs.projecthub.commons.ErrorCode;
-import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,52 +9,57 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import org.springframework.stereotype.Component;
+
+import com.sensilabs.projecthub.commons.ApplicationException;
+import com.sensilabs.projecthub.commons.ErrorCode;
+
 @Component
-public class LocalStorageService implements StorageService{
-    @Override
-    public String save(MultipartFile file) {
-        String path = preparePath() + "/" + file.getOriginalFilename();
-        try {
-            Files.copy(file.getInputStream(), Paths.get(path));
-        } catch (Exception e) {
-            if (e instanceof FileAlreadyExistsException) {
-                throw new RuntimeException("A file of that name already exists.");
-            }
-            throw new RuntimeException(e.getMessage());
-        }
-        return path;
-    }
+public class LocalStorageService implements StorageService {
+	@Override
+	public String save(InputStream inputStream) {
+		String path = preparePath() + "/" + UUID.randomUUID();
+		try {
+			Files.copy(inputStream, Paths.get(path));
+		} catch (Exception e) {
+			if (e instanceof FileAlreadyExistsException) {
+				throw new RuntimeException("A file of that name already exists.");
+			}
+			throw new RuntimeException(e.getMessage());
+		}
+		return path;
+	}
 
-    @Override
-    public byte[] get(String path) {
-        try {
-            return Files.readAllBytes(Path.of(path));
-        } catch (IOException e) {
-            throw new ApplicationException(ErrorCode.FILE_NOT_FOUND);
-        }
-    }
+	@Override
+	public byte[] get(String path) {
+		try {
+			return Files.readAllBytes(Path.of(path));
+		} catch (IOException e) {
+			throw new ApplicationException(ErrorCode.FILE_NOT_FOUND);
+		}
+	}
 
-    public String preparePath() {
-        String basePath = "/attachments";
-        LocalDate currentDate = LocalDate.now();
-        String id = UUID.randomUUID().toString();
-        String folderPath = basePath + "/" + currentDate + "/" + id;
-        try {
-            Path baseFolderPath = Paths.get(basePath);
-            if (!Files.exists(baseFolderPath)) {
-                Files.createDirectories(baseFolderPath);
-            }
-            Path dateFolderPath = Paths.get(basePath, String.valueOf(currentDate));
-            if (!Files.exists(dateFolderPath)) {
-                Files.createDirectories(dateFolderPath);
-            }
-            Path idFolderPath = Paths.get(dateFolderPath.toString(), id);
-            if (!Files.exists(idFolderPath)) {
-                Files.createDirectories(idFolderPath);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to create directory or save file", e);
-        }
-        return folderPath;
-    }
+	public String preparePath() {
+		String basePath = "/attachments";
+		LocalDate currentDate = LocalDate.now();
+		String id = UUID.randomUUID().toString();
+		String folderPath = basePath + "/" + currentDate + "/" + id;
+		try {
+			Path baseFolderPath = Paths.get(basePath);
+			if (!Files.exists(baseFolderPath)) {
+				Files.createDirectories(baseFolderPath);
+			}
+			Path dateFolderPath = Paths.get(basePath, String.valueOf(currentDate));
+			if (!Files.exists(dateFolderPath)) {
+				Files.createDirectories(dateFolderPath);
+			}
+			Path idFolderPath = Paths.get(dateFolderPath.toString(), id);
+			if (!Files.exists(idFolderPath)) {
+				Files.createDirectories(idFolderPath);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException("Failed to create directory or save file", e);
+		}
+		return folderPath;
+	}
 }
