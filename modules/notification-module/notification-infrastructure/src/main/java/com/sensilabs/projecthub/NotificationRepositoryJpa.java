94799e764baa1c53c067ba75d86c5c8b3e797086
BartoszBedyk <bartoszbedyk@gmail.com>
2024-05-07 11:11:37 +0200

    Query update

--- modules/notification-module/notification-infrastructure/src/main/java/com/sensilabs/projecthub/NotificationRepositoryJpa.java
+++ modules/notification-module/notification-infrastructure/src/main/java/com/sensilabs/projecthub/NotificationRepositoryJpa.java
@@ -3,12 +3,16 @@ package com.sensilabs.projecthub;
 import com.sensilabs.projecthub.notification.forms.NotificationChannel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.Instant;
 import java.util.List;
 
 public interface NotificationRepositoryJpa extends JpaRepository<NotificationEntity, String> {
 
-   @Query("SELECT n FROM notification n WHERE n.sent = ?1 AND n.channel = ?4 AND n.numberOfAttempts <= ?3 AND (n.lastAttemptOn <= ?2 OR n.lastAttemptOn is null)")
-List<NotificationEntity> findAllBySentAndLastAttemptOnAndNumberOfAttemptsQuery(boolean sent, Instant lastAttemptOn, int numberOfAttempts, NotificationChannel channel);
+   @Query("SELECT n FROM notification n WHERE n.sent = :sent AND n.channel = :channel AND n.numberOfAttempts <= :numberOfAttempts AND (n.lastAttemptOn <= :lastAttemptOn OR n.lastAttemptOn is null)")
+List<NotificationEntity> findAllBySentAndLastAttemptOnAndNumberOfAttemptsQuery( @Param("sent") boolean sent,
+                                                                                @Param("lastAttemptOn") Instant lastAttemptOn,
+                                                                                @Param("numberOfAttempts") int numberOfAttempts,
+                                                                                @Param("channel") NotificationChannel channel);
 }
