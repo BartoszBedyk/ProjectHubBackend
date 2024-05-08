84a1b29fb85cbabb0d09e707d138b7212bb1ab7c
Kacper <109245559+kacpi-k@users.noreply.github.com>
2024-05-08 13:26:58 +0200

    LoggedUser added

--- commons/domain/src/main/java/com/sensilabs/projecthub/commons/ErrorCode.java
+++ commons/domain/src/main/java/com/sensilabs/projecthub/commons/ErrorCode.java
@@ -2,6 +2,7 @@ package com.sensilabs.projecthub.commons;
 
 public enum ErrorCode {
     USER_NOT_FOUND("User not found"),
+    USER_NOT_LOGGED_IN("User is not logged in"),
     WRONG_PASSWORD("Wrong password"),
     LINK_EXPIRED("Link expired"),
     VALIDATION_EXCEPTION("Invalid data provided"),
